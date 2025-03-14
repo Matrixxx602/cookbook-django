5c9ce829a195ebc0ce3e2d1bc6c5482a4231bb42
matrixxx602 <159135973+Matrixxx602@users.noreply.github.com>
2025-03-14 17:54:51 +0100

    Added main index page - main.html

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('recipes/', views.recipes, name='recipes'),
     path('recipes/details/<int:id>', views.details, name='details'),
 ]
